9dad09172114fa1011156950b6235d64f0286371
RAVICHANDRAKALIKI <k.ravichandrareddy@gmail.com>
2020-05-16 03:14:04 +0530

    Added new models, repositories and bootstrap class

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrap.java
@@ -0,0 +1,54 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrap implements CommandLineRunner {
+
+    private final BookRepository bookRepository;
+    private final AuthorRepository authorRepository;
+    private final PublisherRepository publisherRepository;
+
+    public BootStrap(BookRepository bookRepository,
+                     AuthorRepository authorRepository,
+                     PublisherRepository publisherRepository) {
+        this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Book wingsOfFire = new Book("Wings of Fire", "ISBN000001");
+        Author abdulKalam = new Author("Abdul", "Kalam");
+        wingsOfFire.getAuthorSet().add(abdulKalam);
+        abdulKalam.getBookSet().add(wingsOfFire);
+        authorRepository.save(abdulKalam);
+        bookRepository.save(wingsOfFire);
+
+
+        Book experimentsWithTruth = new Book("My Experiments with Truth", "ISBN000011001");
+        Author gandhi = new Author("Mohandas", "Gandhi");
+        experimentsWithTruth.getAuthorSet().add(gandhi);
+        gandhi.getBookSet().add(experimentsWithTruth);
+        authorRepository.save(gandhi);
+        bookRepository.save(experimentsWithTruth);
+
+        Publisher tataMcgrawHill = new Publisher("Tata Mcgraw Hill", "121 Dickenson Road" ,"Bengaluru" ,"KA" ,"560001");
+        publisherRepository.save(tataMcgrawHill);
+
+        System.out.println("Book Count:"+ bookRepository.count());
+        System.out.println("Author Count:"+ authorRepository.count());
+        System.out.println("Publisher Count:"+ publisherRepository.count());
+
+    }
+}
